2e5998c4d0a9e37d1458dfed35efc4f8f7e1c554
Valentine Ayim <valentineayimodu@gmail.com>
2019-05-28 07:43:06 +0100

    Add files via upload

---
+++ Login Validation/myJavascript.js
@@ -0,0 +1,12 @@
+var username = document.getElementById("user");
+var password = document.getElementById("pass");
+
+function validate() {
+    if (user.value == "" || pass.value == "") {
+        alert("No blank spaces");
+        return false;
+    }
+    else{
+        true;
+    }
+}
